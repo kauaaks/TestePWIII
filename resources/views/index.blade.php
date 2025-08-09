8b6c2e59a486dbd11f6dd49dfd261cc8b09eea87
kauaaks <kaua.mota2@etec.sp.gov.br>
2025-08-08 22:55:30 -0300

    second commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color:#081534;">
+    <div align=center>
+        <img src="https://upload.wikimedia.org/wikipedia/pt/a/ae/Paulist%C3%A3o_Sicred_2025.png" alt="banner" style="width:100% height: 100%">
+    </div>
+       <table align=center>
+        <tr>
+          <th style="color:#ffffff;" ><h2>CAMPEONATO PAULISTAO</h2></th>
+        </tr>
+        <tr>
+          <td style="color:#ffffff; width: 100px; height: 100px; text-align: justify;">
+                CONFIRA OS TIMES DA RODADA 1 ABAIXO:
+            </td>
+          <td style="color:#ffffff; width: 50px; height: 100px;"></td>
+          <td style="color:#ffffff;width: 50px; height: 100px; text-align: justify">
+ 
+            </td>
+        </tr>
+        <tr>
+          <td style="color:#ffffff; text-align: justify"><img src="https://cloudfront-us-east-1.images.arcpublishing.com/newr7/LSIWHKLVM5MALL55ARBUFF5CL4.jpg"></td>
+        </tr>
+      </table>
+    <form style="color:#ffffff;">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" value="nameExample"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" value="lastNameExample">
+    </form>
+</body>
+</html>
